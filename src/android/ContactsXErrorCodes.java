89c45144af70ae07537cd67709b39645c281e49c
T-Ev <tng22@wildcats.unh.edu>
2021-08-11 00:42:11 -0400

    added error codes

--- src/android/ContactsXErrorCodes.java
+++ src/android/ContactsXErrorCodes.java
@@ -4,6 +4,8 @@ public enum ContactsXErrorCodes {
     UnsupportedAction(1),
     WrongJsonObject(2),
     PermissionDenied(3),
+    TooShort(4),
+    MultipleDeleted(5),
     UnknownError(10);
 
     public final int value;
